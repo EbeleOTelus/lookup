687ede3055ef0ccf071bd14c8c4676ac1252e0cc
Ebele <ebele.omanukwue@telus.com>
2023-02-03 21:57:13 -0700

    create routes for navigation links

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,39 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './Components/Header/Header';
-
-import NavBar from './Components/NavBar/NavBar';
- 
+import Header from "./Components/Header/Header";
+// import './App.css';
+ import NavBar from './Components/NavBar/NavBar';
 import { Container } from '@mui/system';
+import Trending from './Components/Trending/Trending';
+import Movies from './Components/Movies/Movies';
+import TvSeries from './Components/TvSeries/TvSeries';
+import Search from './Components/Search/Search';
+import Home from './Components/Home/Home';
 
 
 
-
-function App() {
+export default function App() {
   
   return (
     <>
-    
+    <BrowserRouter>
       <Header/>
       
     <div className="App">
-      feef
+      <Container>
+   
+       <Routes>
+       <Route path='/' element={<Home/>} exact/>
+        <Route path='/trending' element={<Trending/>} exact/>
+        <Route path='/movies' element={<Movies/>} />
+        <Route path='/tvseries' element={<TvSeries/>} />
+        <Route path='/search' element={<Search/>} />
+       </Routes>
+
+        
+      </Container>
     </div>
       <NavBar />
-     
+      </BrowserRouter>
     </>
   );
 }
-
-export default App;
-
