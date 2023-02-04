ece13dedc7f11595f2a17c4726aa1be1386cfdbe
Eszter Egyud <eszter.egyud@telus.com>
2023-02-03 18:30:03 -0700

    Create Header component

---
+++ client/src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+// Header
+import React from 'react'
+
+const Header = () => {
+  return (
+    <span onClick={() => window.scroll(0, 0)} className="Header">
+      FlickPick</span>
+  )
+}
+
+export default Header
