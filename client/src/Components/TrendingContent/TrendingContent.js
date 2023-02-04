bfafb54353144af058ef4cbfb53d398932182ce3
Amina Malik <amina.malik@telus.com>
2023-02-04 15:14:19 -0500

    render images in Trending

--- client/src/Components/TrendingContent/TrendingContent.js
+++ client/src/Components/TrendingContent/TrendingContent.js
@@ -1,9 +1,29 @@
-import React from 'react'
 
-const TrendingContent = () => {
+import "./TrendingContent.css"
+import  { img_300, unavaliable } from "./../images/images.js";
+
+const TrendingContent = ({
+  id,
+  poster,
+  title,
+  date,
+  media_type,
+  vote_average,
+}) => {
+ 
   return (
-    <div>Trending</div>
-  )
-}
+    <div className="media">
+      <img className="poster" 
+      src={poster ? `${img_300}/https://image.tmdb.org/t/p/w300/${poster}` : unavaliable} alt={title}/> 
+    <b className="title">{title}</b>
+    <span className="subTitle">
+      {media_type === "tv" ? "TV Series" : "Movie"}
+    <span className="subTitle">{date}</span>
+    </span>
+    </div>
+  );
 
-export default TrendingContent
+
+};
+
+export default TrendingContent;
