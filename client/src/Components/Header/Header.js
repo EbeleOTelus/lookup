0796bab8f51f7b03fe96e03ee537017c6e71d541
Ebele <ebele.omanukwue@telus.com>
2023-02-08 15:33:52 -0700

    implement Login and signin link

--- client/src/Components/Header/Header.js
+++ client/src/Components/Header/Header.js
@@ -1,15 +1,20 @@
 // Header
-import React from 'react'
-import "./Header.css"
+import React from 'react';
+import "./Header.css";
 
 
 const Header = () => {
   return (
-   <div className="Header-box">
-    <span onClick={() => window.scroll(0, 0)} className="Header">
-      TvHaven</span>
+    <div className="Header-box">
+      <span onClick={() => window.scroll(0, 0)} className="Header">
+        TvHaven</span>
+      <div className='loginClass'>
+        <a href="/login">Login🎬</a>
+        <a href="/signup">SignUp</a>
+      </div >
     </div>
-  )
-}
 
-export default Header
+  );
+};
+
+export default Header;
