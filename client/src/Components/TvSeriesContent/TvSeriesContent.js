e5ce3454d71b6082f997b416242fd602f16f8a3e
Eszter Egyud <eszter.egyud@telus.com>
2023-02-09 01:55:01 -0700

    Add items to title modals

--- client/src/Components/TvSeriesContent/TvSeriesContent.js
+++ client/src/Components/TvSeriesContent/TvSeriesContent.js
@@ -3,68 +3,79 @@ import { img_300, unavaliable } from "./../images/images.js";
 import "./TvSeriesContent.css";
 import YouTubeIcon from '@mui/icons-material/YouTube';
 import React, { useRef, useState } from 'react';
-import { Modal, Button} from 'react-bootstrap';
+import { Modal, Button } from 'react-bootstrap';
 
 
 const TvSeriesContent = ({
-  id,
+  key,
   age,
   cast,
   countries,
   genre,
-  imbdb,
+  imdbID,
   overview,
   posterURLs,
   streamingInfo,
   title,
   year,
+  video,
   imdbRating,
-  imdbLink,
-  video
+  imdbLink
 }) => {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-  
-   const myStyle = { border: "0px", opacity: "1", margin: "0px", padding: "0px", position: "relative" }
- 
+
+  const myStyle = { border: "0px", opacity: "1", margin: "0px", padding: "0px", position: "relative" };
+
   return (
     <>
-    <div className="popular" onClick={handleShow}>
-      
-      <img className="poster"
-        src={posterURLs} alt={title} />
-      <b className="title">{title}</b>
-      <span className="media_type" href={imdbLink}>
-      IMDB Rating: {imdbRating}
-        <span className="media_type">{year}</span>
-      </span>
-    </div>
-   
-  <Modal show={show} onHide={handleClose} size="lg"
-      centered>
+      <div className="popular" onClick={handleShow}>
+
+        <img className="poster"
+          src={posterURLs} alt={title} />
+        <b className="title">{title}</b>
+        <span className="media_type" href={imdbLink}>
+          IMDB Rating: {imdbRating}
+          <span className="media_type">{year}</span>
+        </span>
+      </div>
+
+      <Modal show={show} onHide={handleClose} size="lg"
+        centered>
+
         <Modal.Header closeButton>
-          <Modal.Title className="display-4 text-center">{title}</Modal.Title>
+          <Modal.Title className="modalTitle">{title}</Modal.Title>
+          <a href={`https://www.imdb.com/title/${imdbID}`} target="_blank" rel="noreferrer">
+            <img height="50px" src={"https://m.media-amazon.com/images/G/01/IMDb/BG_rectangle._CB1509060989_SY230_SX307_AL_.png"} alt="Coding Beauty logo"></img>
+          </a>
         </Modal.Header>
+
         <Modal.Body>
-        <p className="lead">{overview}</p>
-        <ul>
-          {cast.join(", ")}
-        </ul>
-        <iframe width="100%" height="315" src={`https://www.youtube.com/embed/${video}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          <ul className="modalBod">OVERVIEW: {overview}</ul>
+          <ul> CAST: {cast.join(", ")} </ul>
+{/* 
+          { Object.keys(streamingInfo).map((key, value) => {
+            return <a href={value.ca.link} target="_blank" rel="noreferrer">
+            {key}
+          </a>
+          } */}
+          
+
+
+          <iframe width="100%" height="315" src={`https://www.youtube.com/embed/${video}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </Modal.Body>
+
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
+
         </Modal.Footer>
       </Modal>
-  </>
+    </>
   );
 };
 
--- client/src/Components/images/images.js
+++ client/src/Components/images/images.js
@@ -6,3 +6,7 @@ export const unavaliable = "https://www.movienewz.com/img/films/poster-holder.jp
 export const unavailableLandscape = "https://www.movienewz.com/img/films/poster-holder.jpg"
 
 export const noPicture = "https://www.movienewz.com/img/films/poster-holder.jpg"
+
+export const IMDBLogoRectangle = "https://m.media-amazon.com/images/G/01/IMDb/BG_rectangle._CB1509060989_SY230_SX307_AL_.png"
+
+export const IMDBLogoSquare = "https://ia.media-imdb.com/images/M/MV5BMTczNjM0NDY0Ml5BMl5BcG5nXkFtZTgwMTk1MzQ2OTE@._V1_.png"
