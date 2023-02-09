4950c3edd8df13447ea285430fd68a28868481e8
Ebele <ebele.omanukwue@telus.com>
2023-02-08 19:50:29 -0700

    render video and details

--- client/src/Components/TrendingContent/TrendingContent.js
+++ client/src/Components/TrendingContent/TrendingContent.js
@@ -1,6 +1,10 @@
 
 import "./TrendingContent.css"
 import  { img_300, unavaliable } from "./../images/images.js";
+import { Button } from "react-bootstrap"
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import React, { useRef } from 'react';
+
 
 const TrendingContent = ({
   id,
@@ -9,11 +13,19 @@ const TrendingContent = ({
   title,
   date,
   media_type,
-  vote_average
+  vote_average,
+  video
   
 }) => {
+
+  
+    const videoRef = useRef(null);
+    const handleClick = () => {
+    
+    }
  
   return (
+    <>
     <div className="popular">
       <img className="poster" 
       src={poster ? `${img_300}/https://image.tmdb.org/t/p/w300/${poster}` : unavaliable} alt={title}/> 
@@ -23,6 +35,18 @@ const TrendingContent = ({
     <span className="media_type">{date}</span>
     </span>
     </div>
+    <span>
+
+     <button onClick={handleClick} href={`https://www.youtube.com/watch?v=${video}`} ><YouTubeIcon/>Trailer</button>
+   
+   </span>
+    {/* <Button
+    variant = "contained"
+    startIcon = {<YouTubeIcon/>}
+    color = "secondary"
+    target = "blank"
+    href = {``}/> */}
+    </>
   );
 
 
--- client/src/Components/TvSeriesContent/TvSeriesContent.js
+++ client/src/Components/TvSeriesContent/TvSeriesContent.js
@@ -1,6 +1,9 @@
-import React from 'react';
+// import React from 'react';
 import { img_300, unavaliable } from "./../images/images.js";
-import "./TvSeriesContent.css"
+import "./TvSeriesContent.css";
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import React, { useRef, useState } from 'react';
+import { Modal, Button} from 'react-bootstrap';
 
 
 const TvSeriesContent = ({
@@ -16,12 +19,23 @@ const TvSeriesContent = ({
   title,
   year,
   imdbRating,
-  imdbLink
+  imdbLink,
+  video
 }) => {
-  
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  // const videoRef = useRef(null);
+  //   const handleClick = (event) => {
+  //   event.preventdefault()
+  //   }
+   const myStyle = { border: "0px", opacity: "1", margin: "0px", padding: "0px", position: "relative" }
  
   return (
-    <div className="popular">
+    <>
+    <div className="popular" onClick={handleShow}>
       
       <img className="poster"
         src={posterURLs} alt={title} />
@@ -30,11 +44,33 @@ const TvSeriesContent = ({
       IMDB Rating: {imdbRating}
         <span className="media_type">{year}</span>
       </span>
-      {/* <h3>{props.age}</h3>
-<h2>{props.cast}</h2> */}
-
     </div>
+    <div>
+  </div>
+  <Modal show={show} onHide={handleClose} size="lg"
+      centered>
+        <Modal.Header closeButton>
+          <Modal.Title className="display-4 text-center">{title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <p className="lead">{overview}</p>
+        <ul>
+          {cast.join(", ")}
+        </ul>
+        <iframe width="100%" height="315" src={`https://www.youtube.com/embed/${video}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+  </>
   );
 };
 
 export default TvSeriesContent;
+
